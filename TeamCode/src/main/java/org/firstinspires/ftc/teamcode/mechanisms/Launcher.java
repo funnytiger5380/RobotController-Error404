cfc8d84daeeded27086478a9430fec57a5771904
Dreyken <starfright22@gmail.com>
2026-02-16 14:19:32 -0800

    67

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Launcher.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.FLOAT;
-
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,49 +14,40 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
 public class Launcher {
     private CRServo leftFeeder;
     private CRServo rightFeeder;
     private String leftFeederName = "leftFeeder";
     private String rightFeederName = "rightFeeder";
 
-
     DcMotorSimple.Direction leftFeederDirection = FORWARD;
     DcMotorSimple.Direction leftFeederDirectionInv = REVERSE;
 
-
     DcMotorSimple.Direction rightFeederDirection = REVERSE;
     DcMotorSimple.Direction rightFeederDirectionInv = FORWARD;
-
 
     private DcMotorEx launcher;
     private String launcherName = "launcher";
     DcMotorSimple.Direction launcherDirection = FORWARD;
     DcMotor.ZeroPowerBehavior laucherZeroPowerBehavior = BRAKE;
 
-
     private double closeTargetVelocity = 1300;
     private double closeMinVelocity    = 1275;
-    private double farTargetVelocity   = 1600;
-    private double farMinVelocity      = 1575;
-
+    private double farTargetVelocity   = 1650;
+    private double farMinVelocity      = 1640;
 
     private double feederRunSec = 0.15;
-    private double launcherCoolOffSec = 0.20;
+    // UPDATED: Set to 2.1 seconds as requested
+    private double launcherCoolOffSec = 2.1;
 
-
-    // === Launcher state machine ===
     private enum LaunchState { IDLE, PANIC, SPIN_UP_F, SPIN_UP_C, LAUNCH, LAUNCHING, RETRACT, COOL_OFF }
     private LaunchState launchState = LaunchState.IDLE;
     private boolean launcherOnAtIdle = false;
     private volatile boolean isBusy = false;
     private volatile boolean isPanic = false;
 
-
     private final ElapsedTime feederTimer = new ElapsedTime();
     private final ElapsedTime launchTimer = new ElapsedTime();
-
 
     public void build(HardwareMap hardwareMap) {
         launcher = hardwareMap.get(DcMotorEx.class, launcherName);
@@ -76,13 +65,6 @@ public class Launcher {
         rightFeeder.setPower(0.0);
     }
 
-
-
-
-
-
-
-
     public Launcher launcherName(String launcherName) {
         this.launcherName = launcherName;
         return this;
@@ -92,18 +74,15 @@ public class Launcher {
         return this;
     }
 
-
     public Launcher rightFeederName(String feederName) {
         this.rightFeederName = feederName;
         return this;
     }
 
-
     public Launcher launcherDirection(DcMotorSimple.Direction motorDirection) {
         launcherDirection = motorDirection;
         return this;
     }
-
 
     public Launcher launcherUseBrakeMode(boolean useBrakeMode) {
         if (useBrakeMode)
@@ -113,13 +92,11 @@ public class Launcher {
         return this;
     }
 
-
     public Launcher leftFeederDirection(DcMotorSimple.Direction motorDirection) {
         leftFeederDirection = motorDirection;
         leftFeederDirectionInv = leftFeederDirection.inverted();
         return this;
     }
-
 
     public Launcher rightFeederDirection(DcMotorSimple.Direction motorDirection) {
         rightFeederDirection = motorDirection;
@@ -127,16 +104,13 @@ public class Launcher {
         return this;
     }
 
-
     public void setMotorDirection(DcMotorSimple.Direction motorDirection) {
         launcher.setDirection(motorDirection);
     }
 
-
     public DcMotorSimple.Direction getMotorDirection() {
         return launcherDirection;
     }
-
 
     public void setLeftFeederDirection(DcMotorSimple.Direction motorDirection) {
         leftFeederDirection = motorDirection;
@@ -144,43 +118,35 @@ public class Launcher {
         leftFeeder.setDirection(leftFeederDirection);
     }
 
-
     public void setRightFeederDirection(DcMotorSimple.Direction motorDirection) {
         rightFeederDirection = motorDirection;
         rightFeederDirectionInv = rightFeederDirection.inverted();
         rightFeeder.setDirection(rightFeederDirection);
     }
 
-
     public DcMotorSimple.Direction getLeftFeederDirection() {
         return leftFeederDirection;
     }
-
 
     public DcMotorSimple.Direction getRightFeederDirection() {
         return rightFeederDirection;
     }
 
-
     public boolean isBusy() {
         return isBusy;
     }
-
 
     public boolean isPanic() {
         return isPanic;
     }
 
-
     public void launcherOnAtIdle() {
         launcherOnAtIdle = true;
     }
 
-
     public void launcherOffAtIdle() {
         launcherOnAtIdle = false;
     }
-
 
     public void setLauncherOff() {
         launcher.setVelocity(0.0);
@@ -191,65 +157,55 @@ public class Launcher {
         launchState = LaunchState.IDLE;
     }
 
-
     public void launchCloseShot() {
         launch(true, false, false);
-        // wait until the current shot is done
         do { launch(false, false, false); } while (isBusy);
     }
-
 
     public void launchCloseShot(int count) {
         launchCloseShot(count, 0.0);
     }
 
-
     public void launchCloseShot(int count, double interval) {
-        launcherOnAtIdle(); // set to keep launcher fly wheel on between multiple launches
+        launcherOnAtIdle();
         for (int i = count; i > 0; i--) {
-            if (i == 1) { // set to launcher fly wheel off after the last launch
+            if (i == 1) {
                 launcherOffAtIdle();
                 launchCloseShot();
             } else {
                 launchCloseShot();
                 launchTimer.reset();
-                do {} while (launchTimer.seconds() < interval); // interval between launches
+                do {} while (launchTimer.seconds() < interval);
             }
         }
     }
 
-
     public void launchFarShot() {
         launch(false, true, false);
-        // wait until the current shot is done
         do { launch(false, false, false); } while (isBusy);
     }
-
 
     public void launchFarShot(int count) {
         launchFarShot(count, 0.0);
     }
 
-
     public void launchFarShot(int count, double interval) {
-        launcherOnAtIdle(); // set to keep launcher fly wheel on between multiple launches
+        launcherOnAtIdle();
         for (int i = count; i > 0; i--) {
-            if (i == 1) { // set to launcher fly wheel off after the last launch
+            if (i == 1) {
                 launcherOffAtIdle();
                 launchFarShot();
             } else {
                 launchFarShot();
                 launchTimer.reset();
-                do {} while (launchTimer.seconds() < interval); // interval between launches
+                do {} while (launchTimer.seconds() < interval);
             }
         }
     }
 
-
     public void launch(boolean closeShot, boolean farShot) {
         launch(closeShot, farShot, false);
     }
-
 
     public void launch(boolean closeShot, boolean farShot, boolean panic) {
         switch (launchState) {
@@ -334,39 +290,43 @@ public class Launcher {
                 }
                 break;
             case COOL_OFF:
-                if (launchTimer.seconds() > launcherCoolOffSec)
+                // Check if trigger is pulled again to reset the cycle
+                if (farShot) {
+                    isBusy = true;
+                    launchState = LaunchState.SPIN_UP_F;
+                } else if (closeShot) {
+                    isBusy = true;
+                    launchState = LaunchState.SPIN_UP_C;
+                }
+                // Wait for the 2.1s timer to expire
+                else if (launchTimer.seconds() > launcherCoolOffSec) {
                     launchState = LaunchState.IDLE;
+                }
                 break;
         }
     }
-
 
     public String getState() {
         return launchState.toString();
     }
 
-
     public double getLauncherVelocity() {
         return launcher.getVelocity();
     }
-
 
     public void setLauncherCloseVelocity(double target, double minimum) {
         closeTargetVelocity = target;
         closeMinVelocity = minimum;
     }
 
-
     public void setLauncherFarVelocity(double target, double minimum) {
         farTargetVelocity = target;
         farMinVelocity = minimum;
     }
 
-
     public void setFeederRunSec(double second) {
         feederRunSec = second;
     }
-
 
     public void setLauncherCoolOffSec(double second) {
         launcherCoolOffSec = second;
